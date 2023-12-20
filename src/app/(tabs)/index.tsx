6fcfc384562aa7b88dd7ecebd64a15673554198c
Nauris Tisancins <nauristisanchins@gmail.com>
2023-12-20 21:27:42 +0000

    api call tes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,26 +5,27 @@ import SelectedRoutineView from '../../components/SelectedRoutineView';
 import { observer } from 'mobx-react';
 
 function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <SelectedRoutineView />
-      <View style={styles.separator} />
-      <RoutineList />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <SelectedRoutineView />
+            <View style={styles.separator} />
+            <RoutineList />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 14,
-    marginVertical: 30,
-    gap: 30,
-  },
-  separator: {
-    borderBottomColor: 'lightgray',
-    borderBottomWidth: 0.5,
-    marginHorizontal: 10,
-  },
+    container: {
+        backgroundColor: 'black',
+        marginHorizontal: 14,
+        marginVertical: 30,
+        gap: 30,
+    },
+    separator: {
+        borderBottomColor: 'lightgray',
+        borderBottomWidth: 0.5,
+        marginHorizontal: 10,
+    },
 });
 
 export default observer(HomeScreen);
--- src/components/RoutineList.tsx
+++ src/components/RoutineList.tsx
@@ -1,34 +1,64 @@
 import { StyleSheet } from 'react-native';
-import { View } from '../components/Themed';
+import { View, Text } from '../components/Themed';
 import RoutineView from './RoutineListItem';
 import { useStore } from '../store/index';
 import { observer } from 'mobx-react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const RoutineList = () => {
-  const { routinesList, selectedRoutine } = useStore();
+    const { routinesList, selectedRoutine } = useStore();
+    const [response, setResponse] = useState(null);
 
-  return (
-    <View style={styles.container}>
-      {routinesList
-        .filter((routine) => {
-          if (selectedRoutine) {
-            return routine.id !== selectedRoutine.id;
-          }
-          return routine;
-        })
-        .map((item) => {
-          return <RoutineView key={item.id} routine={item} />;
-        })}
-    </View>
-  );
+    async function getUsers() {
+        if (process.env.EXPO_PUBLIC_API_URL) {
+            try {
+                const response = await axios.get(
+                    process.env.EXPO_PUBLIC_API_URL
+                );
+                const data = await response.data;
+                if (data) {
+                    console.log(data);
+                    setResponse(data);
+                }
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.test}>{JSON.stringify(response, null, 2)}</Text>
+            {routinesList
+                .filter((routine) => {
+                    if (selectedRoutine) {
+                        return routine.id !== selectedRoutine.id;
+                    }
+                    return routine;
+                })
+                .map((item) => {
+                    return <RoutineView key={item.id} routine={item} />;
+                })}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 20,
-  },
+    test: {
+        color: 'red',
+    },
+
+    container: {
+        backgroundColor: 'black',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 20,
+    },
 });
 
 export default observer(RoutineList);
